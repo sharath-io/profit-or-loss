58c7d8b95adf082ba85ebd1f9fb903e4e1278cf2
Sharath <sharath.esr@gmail.com>
2021-09-30 15:13:48 +0530

    feat: added caution message to the user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var currentPrice = document.querySelector("#current-price");
 
 var calculateBtn = document.querySelector("#calculate-btn");
 var outputDiv = document.querySelector("#output-div");
+var cautionDiv = document.querySelector("#caution-msg");
 
 
 
@@ -13,13 +14,16 @@ function calculateProfitOrLoss(initial, quantity, current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss / initial) * 100;
         outputDiv.innerText = " the loss is : " + loss + " and the loss percentage is " + lossPercentage;
+        cautionDiv.innerText = "Dont think, as you are big bull , invest cautiously ";
     } else if (current > initial) {
         //profit
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100;
         outputDiv.innerText = " the profit is: " + profit + " and the profit percentage is " + profitPercentage;
+        cautionDiv.innerText =" Congrats, but stock market is fluid dont go with the flow";
     } else {
         outputDiv.innerText = "no loss / no profit";
+        cautionDiv.innerText ="Sorry, you have no luck better go with FD";
     }
 }
 
